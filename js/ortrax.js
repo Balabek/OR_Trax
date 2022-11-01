75cbb780b97831976dd1b4a93429246d99103dd9
Balabek <vonbabayan@gmail.com>
2022-11-01 12:54:21 +0300

    Homepage (fully responsive)

---
+++ js/ortrax.js
@@ -0,0 +1,24 @@
+/* 𝙲𝚞𝚜𝚝𝚘𝚖 𝙹𝚂 */
+
+$(document).ready(function(){
+
+    // Fancy animation for hamburger menu
+    $('.ortrx_nav_toggler').on('click', function(){
+        $('.animated-icon1').toggleClass('open');
+    });
+
+
+
+    // Subtle fade-in animation for the below-mentioned elements
+    // ScrollReveal().reveal('.solution_box_outermost', {delay: 100});
+    // ScrollReveal().reveal('.twophones', {delay: 500, duration: 1400});
+    // ScrollReveal().reveal('.circle_icon_holder', {delay: 300, duration: 800});
+    // ScrollReveal().reveal('.statbox', {delay: 400});
+    // ScrollReveal().reveal('.popbox_area', {delay: 600, duration: 600});
+    //
+    // ScrollReveal().reveal('.team_member', {delay: 150});
+    //
+    // ScrollReveal().reveal('.ipad_collage_holder', {delay: 200, duration: 600});
+
+
+});
